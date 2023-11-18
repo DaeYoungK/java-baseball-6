5346ee8995d0ace7325a4128051be2cf49d844e5
DaeYoungK <hanguk830@naver.com>
2023-11-19 00:22:38 +0900

    fix(Validator): 길이 검증 시 파라미터로 원하는 길이 체크하도록 변경

--- src/main/java/baseball/validation/Validator.java
+++ src/main/java/baseball/validation/Validator.java
@@ -2,15 +2,18 @@ package baseball.validation;
 
 public class Validator {
 
+    private static final int PLAYER_NUMBER = 3;
+    private static final int RETRY_NUMBER = 1;
+
     public int playerNumber(String number) {
-        checkSize(number);
+        checkSize(number, PLAYER_NUMBER);
         int resultNumber = checkType(number);
 
         return resultNumber;
     }
 
-    private void checkSize(String number) {
-        if (number.length() != 3) {
+    private void checkSize(String number, int length) {
+        if (number.length() != length) {
             throw new IllegalArgumentException();
         }
     }
