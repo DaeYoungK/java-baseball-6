03c05d9a04d1730dcad912e9db9ea616992a1888
DaeYoungK <hanguk830@naver.com>
2023-11-18 23:07:14 +0900

    feat(Hint): 스트라이크와 볼 개수를 받아 힌트 메시지 생성 기능 추가

---
+++ src/main/java/baseball/domain/Hint.java
@@ -0,0 +1,23 @@
+package baseball.domain;
+
+public class Hint {
+
+    private static final String STRIKE_MESSAGE = "스트라이크";
+    private static final String BALL_MESSAGE = "볼";
+    private static final String NOTHING_MESSAGE = "낫싱";
+    private static final String BLANK = " ";
+
+    public String create(int strikeCount, int ballCount) {
+        StringBuilder sb = new StringBuilder();
+        if (strikeCount == 0 && ballCount != 0) {
+            return sb.append(ballCount + BALL_MESSAGE).toString();
+        }
+        if (strikeCount != 0 && ballCount == 0) {
+            return sb.append(strikeCount + STRIKE_MESSAGE).toString();
+        }
+        if (strikeCount != 0 && ballCount != 0) {
+            return sb.append(ballCount + BALL_MESSAGE + BLANK + strikeCount + STRIKE_MESSAGE).toString();
+        }
+        return NOTHING_MESSAGE;
+    }
+}
