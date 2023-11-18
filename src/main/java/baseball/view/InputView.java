faabf8b069c1dc8436ffda4565a1e0595d447664
DaeYoungK <hanguk830@naver.com>
2023-11-18 21:28:06 +0900

    feat(InputView): 사용자가 입력하는 기능 추가

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,19 @@
+package baseball.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String INPUT_RETRY_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public int readNumber() {
+        System.out.print(INPUT_NUMBER_MESSAGE);
+        return Integer.parseInt(Console.readLine());
+    }
+
+    public int readRetryNumber() {
+        System.out.println(INPUT_RETRY_MESSAGE);
+        return Integer.parseInt(Console.readLine());
+    }
+}
