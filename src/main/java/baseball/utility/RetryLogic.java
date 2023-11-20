6136b8f2754463c6c8b2a2d546fe4fde5f63e710
DaeYoungK <hanguk830@naver.com>
2023-11-20 20:16:37 +0900

    fix(RetryLogic): 정상로직 실행시 반복문 탈출

--- src/main/java/baseball/utility/RetryLogic.java
+++ src/main/java/baseball/utility/RetryLogic.java
@@ -5,20 +5,21 @@ public class RetryLogic {
     private static final int MAX_RETRY = 5;
 
     public static void retry(Retry retry) {
-        int count = 1;
         for (int i = 1; i <= MAX_RETRY; i++) {
-            logic(retry, i);
+            if (isLogic(retry, i)) break;
         }
 
     }
 
-    private static void logic(Retry retry, int i) {
+    private static boolean isLogic(Retry retry, int i) {
         try {
             retry.run();
+            return true;
         }catch (IllegalArgumentException e) {
             if (i == MAX_RETRY) {
                 throw e;
             }
         }
+        return false;
     }
 }
